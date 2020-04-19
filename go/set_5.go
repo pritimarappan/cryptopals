eb5fb99a95b151a65d7edad6193cf150ca37ee0f
Priti <marappanpriti@linkedin.com>
2020-04-18 17:59:28 -0700

    prob 34 in progress

--- go/set_5.go
+++ go/set_5.go
@@ -2,6 +2,8 @@ package cryptopals
 
 import (
 	"crypto/rand"
+	cryptosha1 "crypto/sha1"
+	"fmt"
 	"math/big"
 )
 
@@ -13,11 +15,11 @@ type dhKeys struct {
 	pvt, pub *big.Int
 }
 
-func (dhk *dhKeys) getDhPublicKey(dhp *dhParams) {
+func (dhk *dhKeys) setDhPublicKey(dhp *dhParams) {
 	dhk.pub = new(big.Int).Exp(dhp.g, dhk.pvt, dhp.p)
 }
 
-func (dhk *dhKeys) getDhPrivateKey(p *big.Int) {
+func (dhk *dhKeys) setDhPrivateKey(p *big.Int) {
 	pvt, err := rand.Int(rand.Reader, p)
 	if err != nil {
 		panic(err)
@@ -28,4 +30,29 @@ func (dhk *dhKeys) getDhPrivateKey(p *big.Int) {
 func getDhSecret(p *big.Int, pub *big.Int, pvt *big.Int) *big.Int {
 	s := new(big.Int).Exp(pub, pvt, p)
 	return s
+}
+
+type dhEchoBot struct {
+	dhp    *dhParams
+	dhk    *dhKeys
+	Secret *big.Int
+}
+
+func (dhBot *dhEchoBot) init(dhp *dhParams, pubA *big.Int) {
+	dhBot.dhp = dhp
+	dhBot.dhk = new(dhKeys)
+	dhBot.dhk.setDhPrivateKey(dhp.p)
+	dhBot.dhk.setDhPublicKey(dhp)
+	dhBot.Secret = getDhSecret(dhp.p, pubA, dhBot.dhk.pvt)
+}
+
+func (dhBot *dhEchoBot) echo(in []byte) []byte {
+	hash := cryptosha1.Sum(dhBot.Secret.Bytes())
+	key := hash[:16]
+	pt := aesCbcDecrypt(in[:len(in)-16], key, in[len(in)-16:])
+	fmt.Println(string(pt))
+	iv := generateRandomBytes(16)
+	ct := aesCbcEncrypt(pt, key, iv)
+	return append(ct, iv...)
+
 }
--- go/set_5_test.go
+++ go/set_5_test.go
@@ -1,6 +1,7 @@
 package cryptopals
 
 import (
+	cryptosha1 "crypto/sha1"
 	"fmt"
 	"math/big"
 	"testing"
@@ -15,11 +16,45 @@ func Test33(t *testing.T) {
 	dhp.p = p
 	dha := new(dhKeys)
 	dhb := new(dhKeys)
-	dha.getDhPrivateKey(dhp.p)
-	dha.getDhPublicKey(dhp)
-	dhb.getDhPrivateKey(dhp.p)
-	dhb.getDhPublicKey(dhp)
+	dha.setDhPrivateKey(dhp.p)
+	dha.setDhPublicKey(dhp)
+	dhb.setDhPrivateKey(dhp.p)
+	dhb.setDhPublicKey(dhp)
 	sa := getDhSecret(dhp.p, dhb.pub, dha.pvt)
 	sb := getDhSecret(dhp.p, dha.pub, dha.pvt)
 	fmt.Println(sa.Cmp(sb))
+}
+
+func Test34(t *testing.T) {
+	var p, _ = new(big.Int).SetString("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", 16)
+	g := big.NewInt(2)
+
+	dhp := new(dhParams)
+	dhp.g = g
+	dhp.p = p
+
+	dha := new(dhKeys)
+	dha.setDhPrivateKey(dhp.p)
+	dha.setDhPublicKey(dhp)
+
+	dhBot := new(dhEchoBot)
+	dhBot.init(dhp, dha.pub)
+
+	B := dhBot.dhk.pub
+
+	sA := getDhSecret(dhp.p, B, dha.pvt)
+
+	iv := generateRandomBytes(16)
+	hash := cryptosha1.Sum(sA.Bytes())
+	key := hash[:16]
+	msg := []byte("Hello world")
+	msg = pkcs7Padding(msg, BLOCKSIZE)
+	ct := aesCbcEncrypt(msg, key, iv)
+
+	echoedCt := dhBot.echo(append(ct, iv...))
+
+	echoedPt := aesCbcDecrypt(echoedCt[:len(echoedCt)-16], key, echoedCt[len(echoedCt)-16:])
+	echoedPt, _ = pkcs7UnPadding(echoedPt)
+	fmt.Println(string(echoedPt))
+
 }
